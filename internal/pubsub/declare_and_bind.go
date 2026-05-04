a1f298e34d473a484539fe61b659efbb5f243f73
Brando-1212 <birkelobrandon@gmail.com>
2026-05-04 14:17:35 -0500

    B. Added transient queue and resume, pause, quit game states

---
+++ internal/pubsub/declare_and_bind.go
@@ -0,0 +1,52 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Acktype int
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // SimpleQueueType is an "enum" type I made to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("couldn't create channel: %v", err)
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,
+		queueType == SimpleQueueDurable,
+		queueType != SimpleQueueDurable,
+		queueType != SimpleQueueDurable,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("couldn't create queue: %v", err)
+	}
+	err = ch.QueueBind(
+		queue.Name,
+		key,
+		exchange,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("coundn't bind queue: %v", err)
+	}
+	return ch, queue, nil
+}
